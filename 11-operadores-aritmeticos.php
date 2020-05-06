68fa22e37b460608c0f75b456ad2d0afd48d1db6
Borges <gustavoborges98@hotmail.com>
2020-05-05 21:35:52 -0300

    Operadores Aritmeticos e Incrmento/Decremento

---
+++ 11-operadores-aritmeticos.php
@@ -0,0 +1,48 @@
+<?php
+
+
+//Operadores aritmeticos:
+
+echo 10 + 10;
+echo "<br>";
+echo 10 - 5;
+echo "<br>";
+echo 3 * 5;
+echo "<br>";
+echo 20 / 4;
+echo "<br>";
+echo 15 % 2;
+echo "<br>";
+echo 3 ** 5;
+echo "<hr>";
+
+$a = 10;
+$b = 20;
+$c = 30;
+$d = 5;
+
+$adicao = $a + $b;
+echo $adicao;
+echo "<hr>";
+
+$subtracao = $c - $a;
+echo $subtracao;
+echo "<hr>";
+
+$multiplicacao = $c * $a;
+echo $multiplicacao;
+echo "<hr>";
+
+$divisao = $c / $a;
+echo $divisao;
+echo "<hr>";
+
+$resto = $c % $a;
+echo $resto;
+echo "<hr>";
+
+$explonencial = $c ** $a;
+echo $explonencial;
+echo "<hr>";
+
+echo (5 + 7 + 9 + 8) / 4;
---
+++ 12-operaodres_incremento_decremento.php
@@ -0,0 +1,23 @@
+<?php
+
+//pré-incremento:
+$valor = 20;
+echo ++$valor;
+echo "<hr>";
+
+//pós-incrmeento:
+echo $valor++;
+echo "<hr>";
+
+echo $valor;
+echo "<hr>";
+
+
+//pré e pós decremento:
+echo --$valor;
+echo "<br>";
+echo $valor;
+echo "<br>";
+echo $valor++;
+echo "<br>";
+echo $valor;
