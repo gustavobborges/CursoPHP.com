ed2b61d1aae372e38564aedf4d4337ef3d1759b5
Borges <gustavoborges98@hotmail.com>
2020-05-06 23:36:52 -0300

    Superglobais e primeiras entradas de dados

---
+++ 22-formularios/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="dados.php" method="GET">
+Nome: <input type="text" name="nome"><br>
+Email: <input type="email" name="email"><br>
+<button type="submit">Enviar</button><br>
+</form>
+
+
+<a href="dados.php?idade=25&sobrenome=Borges">Enviar Dados</a>
+
+</body>
+</html>
